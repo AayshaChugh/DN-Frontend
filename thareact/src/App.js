332142b227cddd5c82cc715794bd7d0451d740af
Aaysha <chughaaysha@gmail.com>
2021-07-13 13:25:39 +0530

    ChessBoard Added

--- thareact/src/App.js
+++ thareact/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import MemeCard from "./memeCard/MemeCard";
+import ChessBoard from "./chessBoard/chessBoard";
 function App() {
   return (
     <div className="App">
-      <MemeCard />
+      {/* <MemeCard /> */}
+      <ChessBoard />
     </div>
   );
 }
---
+++ thareact/src/chessBoard/chessBoard.jsx
@@ -0,0 +1,25 @@
+import "./chessBoard.css";
+
+const ChessBoard = () => {
+  let boxes = [];
+  for (let i = 0; i < 8; i++) {
+    boxes.push(<div className="chessSquare" />);
+  }
+  console.log(boxes);
+  return (
+    <div className="chessApp">
+      <div className="chessBoard">
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+        <div className="chessRow">{boxes}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ChessBoard;
